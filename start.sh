c8d99b2430aa2811d4aa4b53a0b4210a1dbc3445
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-02-01 20:27:57 +0200

    T-6 | create service discovery

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml down
 bash ./core/build.sh
+bash ./system/service-discovery/build.sh
 docker-compose -f docker-compose.yml up -d
--- system/service-discovery/src/main/java/com/harera/hayat/serviceregistery/ServiceDiscoveryApplication.java
+++ system/service-discovery/src/main/java/com/harera/hayat/serviceregistery/ServiceDiscoveryApplication.java
@@ -1,15 +1,14 @@
 package com.harera.hayat.serviceregistery;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        org.springframework.boot.SpringApplication.run(ServiceDiscoveryApplication.class, args);
+        org.springframework.boot.SpringApplication.run(ServiceDiscoveryApplication.class,
+                        args);
     }
 }
