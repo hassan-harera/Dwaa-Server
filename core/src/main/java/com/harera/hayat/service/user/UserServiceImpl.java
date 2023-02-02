0d4fe6d84f0b2f618de67e5df7ac693f7deb8276
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-02-02 08:47:06 +0200

    T-5 | Merged with master

---
+++ core/src/main/java/com/harera/hayat/service/user/UserServiceImpl.java
@@ -0,0 +1,149 @@
+package com.harera.hayat.service.user;
+
+import static com.harera.hayat.util.RegexUtils.isEmail;
+import static com.harera.hayat.util.RegexUtils.isPhoneNumber;
+import static com.harera.hayat.util.RegexUtils.isUsername;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.harera.hayat.exception.SignupException;
+import com.harera.hayat.model.user.FirebaseUser;
+import com.harera.hayat.model.user.User;
+import com.harera.hayat.model.user.auth.LoginRequest;
+import com.harera.hayat.model.user.auth.LoginResponse;
+import com.harera.hayat.model.user.auth.LogoutRequest;
+import com.harera.hayat.model.user.auth.OAuthLoginRequest;
+import com.harera.hayat.model.user.auth.SignupRequest;
+import com.harera.hayat.model.user.auth.SignupResponse;
+import com.harera.hayat.repository.UserRepository;
+import com.harera.hayat.repository.user.auth.TokenRepository;
+import com.harera.hayat.service.firebase.FirebaseService;
+import com.harera.hayat.service.user.auth.AuthService;
+import com.harera.hayat.service.user.auth.JwtService;
+import com.harera.hayat.service.user.auth.JwtUtils;
+
+@Service
+class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserValidation userValidation;
+    private final TokenRepository tokenRepository;
+    private final FirebaseService firebaseService;
+    private final ModelMapper modelMapper;
+    private final JwtUtils jwtUtils;
+    private final JwtService jwtService;
+
+    UserServiceImpl(UserRepository userRepository, AuthService authService,
+                    PasswordEncoder passwordEncoder, UserValidation userValidation,
+                    TokenRepository tokenRepository, FirebaseService firebaseService,
+                    ModelMapper modelMapper, JwtUtils jwtUtils, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.authService = authService;
+        this.passwordEncoder = passwordEncoder;
+        this.userValidation = userValidation;
+        this.tokenRepository = tokenRepository;
+        this.firebaseService = firebaseService;
+        this.modelMapper = modelMapper;
+        this.jwtUtils = jwtUtils;
+        this.jwtService = jwtService;
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        userValidation.validate(loginRequest);
+
+        long userId = getUserId(loginRequest.getSubject());
+        User user = userRepository.findById(userId).orElseThrow(
+                        () -> new UsernameNotFoundException("User not found"));
+
+        if (!Objects.equals(user.getDeviceToken(), loginRequest.getDeviceToken())) {
+            user.setDeviceToken(loginRequest.getDeviceToken());
+            userRepository.save(user);
+        }
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(jwtService.generateToken(user));
+        loginResponse.setRefreshToken(jwtService.generateRefreshToken(user));
+
+        return loginResponse;
+    }
+
+    public LoginResponse login(OAuthLoginRequest oAuthLoginRequest) {
+        userValidation.validate(oAuthLoginRequest);
+        //TODO
+        return null;
+    }
+
+    public SignupResponse signup(SignupRequest signupRequest) {
+        userValidation.validate(signupRequest);
+        FirebaseUser firebaseUser = firebaseService.createUser(signupRequest);
+        if (firebaseUser == null) {
+            throw new SignupException("User creation failed");
+        }
+
+        User user = modelMapper.map(signupRequest, User.class);
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        user.setUid(firebaseUser.getUid());
+        user.setUsername(firebaseUser.getUid());
+
+        userRepository.save(user);
+        return modelMapper.map(user, SignupResponse.class);
+    }
+
+    private long getUserId(String subject) {
+        long userId = 0;
+        if (isPhoneNumber(subject)) {
+            userId = userRepository.findByMobile(subject).get().getId();
+        } else if (isEmail(subject)) {
+            userId = userRepository.findByEmail(subject).get().getId();
+        } else if (isUsername(subject)) {
+            userId = userRepository.findByUsername(subject).get().getId();
+        }
+        return userId;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+                    throws UsernameNotFoundException {
+        try {
+            long userId = Integer.parseInt(username);
+            return userRepository.findById(userId).orElse(null);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    public LoginResponse refresh(String refreshToken) {
+        String usernameOrMobile = jwtUtils.extractUserSubject(refreshToken);
+        final User user = (User) loadUserByUsername(usernameOrMobile);
+        jwtUtils.validateRefreshToken(user, refreshToken);
+        LoginResponse authResponse = new LoginResponse();
+        authResponse.setToken(jwtService.generateToken(user));
+        authResponse.setRefreshToken(jwtService.generateRefreshToken(user));
+        return authResponse;
+    }
+
+    public void logout(LogoutRequest request) {
+        String usernameOrMobile = jwtUtils.extractUserSubject(request.getToken());
+        final User user = (User) loadUserByUsername(usernameOrMobile);
+        // resetting user device token in case of logout
+        if (StringUtils.isNotEmpty(user.getDeviceToken())) {
+            user.setDeviceToken(null);
+            userRepository.save(user);
+        }
+        jwtUtils.validateToken(user, request.getToken());
+        tokenRepository.removeUserToken(request.getToken());
+        if (StringUtils.isNotEmpty(request.getRefreshToken())) {
+            jwtUtils.validateRefreshToken(user, request.getRefreshToken());
+            tokenRepository.removeUserRefreshToken(request.getRefreshToken());
+        }
+    }
+}
